ac50336f65d6190787b0a6f98544fc3e90bff3c9
Sebastian <sebastian.gonzalez.cass@usach.cl>
2023-07-25 20:43:45 -0400

    feat: se inicializa proyecto de spring

---
+++ api/src/main/java/libreriadelibros/libreriadelibros_api/LibreriadelibrosApiApplication.java
@@ -0,0 +1,13 @@
+package libreriadelibros.libreriadelibros_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriadelibrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriadelibrosApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/libreriadelibros/libreriadelibros_api/LibreriadelibrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package libreriadelibros.libreriadelibros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriadelibrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
